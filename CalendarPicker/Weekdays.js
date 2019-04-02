347e329dff2b6763f7aa3b756b470307b53709b8
Saad <saad@socar.my>
2019-04-02 15:06:53 +0800

    today day label style added

--- CalendarPicker/Weekdays.js
+++ CalendarPicker/Weekdays.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { Utils } from './Utils';
+import moment from 'moment';
 
 export default function Weekdays(props) {
-  const { styles, startFromMonday, weekdays, textStyle } = props;
-  let wd = weekdays;
-  if (!wd) {
-    wd = startFromMonday? Utils.WEEKDAYS_MON : Utils.WEEKDAYS; // English Week days Array
-  }
+	const { styles, startFromMonday, weekdays, textStyle } = props;
+	let wd = weekdays;
+	if (!wd) {
+		wd = startFromMonday ? Utils.WEEKDAYS_MON : Utils.WEEKDAYS; // English Week days Array
+	}
 
-  return (
-    <View style={styles.dayLabelsWrapper}>
-      { wd.map((day, key) => {
-          return (
-            <Text key={key} style={[styles.dayLabels, textStyle]}>
-              {day}
-            </Text>
-          );
-        })
-      }
-    </View>
-  );
+	const today = moment();
+	const dayLabel = today.format('ddd');
+	let cus_wd = Utils.CUS_WEEKDAYS;
+
+	return (
+		<View style={styles.dayLabelsWrapper}>
+			{cus_wd.map((day, key) => {
+				return (
+					<Text
+						key={key}
+						style={[styles.dayLabels, textStyle, dayLabel === day.key && styles.selectedDayCusLabel]}
+					>
+						{day.label}
+					</Text>
+				);
+			})}
+		</View>
+	);
 }
 
 Weekdays.propTypes = {};
